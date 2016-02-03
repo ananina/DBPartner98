3c09780d29a593da62ba6685b1675ebece1360b3
ananina <e.ananina@inbox.ru>
2016-02-03 15:03:49 +0700

    Подключены JQuery и Bootstrap

--- view/base.php
+++ view/base.php
@@ -2,7 +2,10 @@
 <html>
 <head>
     <title><?=$title?></title>
-<meta charset="utf-8">
+    <meta charset="utf-8">
+    <script src="/DBPartner98.git/assets/jquery/jquery-1.11.3.min.js"></script>
+    <script src="/DBPartner98.git/assets/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/DBPartner98.git/assets/bootstrap-3.3.6-dist/css/bootstrap.min.css">
 </head>
 <body>
 <?=$content?>
